85265e32808cdafb62762c403fd5321758f8cc16
kambojsaab300 <mohitkamboj300@gmail.com>
2025-05-04 11:21:04 +0530

    new code

--- hello.py
+++ hello.py
@@ -1,13 +1,7 @@
 print('hello world'):
-print('hello world'):
-print('hello world'):
-print('hello world'):
-print('hello world'):
-print('hello world'):
-print('hello world'):
-print('hello world'):
-print('hello world'):
-print('hello world'):
+
+print('hello world 2')
+
 
 
 # git push  origin main
